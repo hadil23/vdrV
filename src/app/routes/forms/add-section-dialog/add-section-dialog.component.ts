3042e02cfb8e8e7b38a8f503757a296532a66f07
hadil23 <120094233+hadil23@users.noreply.github.com>
2024-08-07 01:27:42 +0100

    resolve autoSave issues

--- src/app/routes/forms/add-section-dialog/add-section-dialog.component.ts
+++ src/app/routes/forms/add-section-dialog/add-section-dialog.component.ts
@@ -3,6 +3,8 @@ import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { VirtualDataRoomComponent } from '../virtual-data-room/virtual-data-room.component';
+import { VirtualRoomService } from '../services/virtual-room.service';
 
 @Component({
   selector: 'app-add-section-dialog',
@@ -11,6 +13,7 @@ import { FormsModule } from '@angular/forms';
   styleUrls: ['./add-section-dialog.component.scss'],
   imports: [
     MatFormFieldModule,
+    VirtualDataRoomComponent,
     MatInputModule,
     MatDialogModule,
     FormsModule
@@ -19,9 +22,36 @@ import { FormsModule } from '@angular/forms';
 export class AddSectionDialogComponent {
   newPanelTitle: string = '';
 
-  constructor(public dialogRef: MatDialogRef<AddSectionDialogComponent>) {}
+  constructor(public dialogRef: MatDialogRef<AddSectionDialogComponent> , private virtualRoomService:VirtualRoomService) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  private panelId!: string; 
+
+  createPanel(): void {
+    this.virtualRoomService.getVirtualRoomId().subscribe(virtualRoomId => {
+      console.log('Virtual Room ID:', virtualRoomId);
+      if (virtualRoomId === null) {
+        console.error('LID de la salle virtuelle est null');
+      } else {
+        const panelTitle = this.newPanelTitle.trim();
+        console.log('Panel Title:', panelTitle);
+        if (panelTitle !== '') {
+          this.virtualRoomService.createPanel(virtualRoomId.toString(), panelTitle).subscribe(
+            response => {
+              console.log('Panel créé avec succès :', response);
+              this.panelId = response.id; // store the panel ID
+            },
+            error => {
+              console.error('Erreur lors de la création du panel :', error);
+            }
+          );
+        } else {
+          console.error('Le titre du panel est vide');
+        }
+      }
+    });
   }
 }
---
+++ src/app/routes/sessions/register/auth.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
+export interface User {
+    id?: number; 
+    name: string;
+    email: string;
+    password: string; 
+    role?: string; 
+   
+  }
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl =" http://localhost:3000/api";
+  private backenUrl = `${this.apiUrl}/users/users`; 
+
+  constructor(private http: HttpClient) {}
+
+  // Inscription de l'utilisateur
+  register(user: User): Observable<User> {
+    return this.http.post<User>(`${this.backenUrl}`, user).pipe(
+      tap((newUser: User) => console.log(`Utilisateur enregistré avec l'ID=${newUser.id}`)),
+      catchError(this.handleError<User>('register'))
+    );
+  }
+
+  // Connexion de l'utilisateur
+  login(email: string, password: string): Observable<any> {
+    return this.http.post<any>(`${this.backenUrl}/login`, { email, password }).pipe(
+      tap(response => console.log('Connexion réussie')),
+      catchError(this.handleError<any>('login'))
+    );
+  }
+
+  // Vérifier si l'utilisateur est authentifié
+  isLoggedIn(): boolean {
+    // Implémentez la logique pour vérifier si l'utilisateur est authentifié
+    // Exemple : vérifiez la présence d'un token dans le stockage local
+    return !!localStorage.getItem('authToken');
+  }
+
+  // Méthode d'erreur générique
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(`${operation} échoué: ${error.message}`);
+      return of(result as T);
+    };
+  }
+}
